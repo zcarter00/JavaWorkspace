6db1a164f855f19b2c24cfa4014b22463937ce92
ceb-student <CEB-Student@caddoschools.org>
2017-08-25 09:18:15 -0500

    added scanner
    scanner added

--- Experimental/src/basicInputOutput/controller/BasicIOController.java
+++ Experimental/src/basicInputOutput/controller/BasicIOController.java
@@ -1,13 +1,21 @@
 package basicInputOutput.controller;
-
+import java.util.Scanner;
 public class BasicIOController 
 {
+	private String firstName = "";
+	private String lastName = "";
+	
+	public BasicIOController(String first, String last) //constructor - method with same name as its class that initializes the class
+	{
+		firstName = first;
+		lastName = last;
+	}
 	public void printGreeting() //parentheses always go after methods
 	{
-		String firstName = "Zach";
-		String lastName = "Carter";
-		System.out.println(firstName + " " + lastName);
-		
+		System.out.println("How do you do, " + firstName + " " + lastName);
+		Scanner myScanner = new Scanner(System.in);
+		String response = myScanner.nextLine();
+		System.out.println(response);
 		
 	}
 }
--- Experimental/src/basicInputOutput/controller/BasicIORunner.java
+++ Experimental/src/basicInputOutput/controller/BasicIORunner.java
@@ -2,10 +2,10 @@ package basicInputOutput.controller;
 
 public class BasicIORunner 
 {
-
+	
 	public static void main(String[] args) 
 	{
-		BasicIOController myGreeting = new BasicIOController();
+		BasicIOController myGreeting = new BasicIOController("John", "Doe");
 		myGreeting.printGreeting();
 
 	}
