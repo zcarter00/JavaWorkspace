ab2dcb51c6034770149b6478b8c9aaa7148f237d
ceb-student <CEB-Student@caddoschools.org>
2017-08-29 09:18:14 -0500

    Overloaded Contructor
     created an overloaded constructor

--- BetterJPAMs/src/stduentManagement/model/Student.java
+++ BetterJPAMs/src/stduentManagement/model/Student.java
@@ -2,16 +2,17 @@ package stduentManagement.model;
 
 public class Student 
 {
-	boolean isCurrentStudent;
-	int gradeLevel;
-	int idNumber;
-	String name;
-	String gender;
-	int age;
-	double gpa;
+	private boolean isCurrentStudent;
+	private int gradeLevel;
+	private int idNumber;
+	private String name;
+	private String gender;
+	private int age;
+	private double gpa;
 	
-	public Student()
+	public Student() 
 	{
+		//this is constructor method; is name of class, initiates instance variables.
 		isCurrentStudent = false;
 		gradeLevel = 0;
 		idNumber = 0;
@@ -20,11 +21,37 @@ public class Student
 		age = 0;
 		gpa = 0.0;
 	}
+	/**
+	 * 
+	 * @param isCurrentStudent
+	 * @param gradeLevel
+	 * @param idNumber
+	 * @param name
+	 * @param gender
+	 * @param age
+	 * @param gpa
+	 */
+	public Student(boolean isCurrentStudent, int gradeLevel, int idNumber, String name, String gender, int age, double gpa)
+	{
+		this.isCurrentStudent = isCurrentStudent;
+		this.gradeLevel = gradeLevel;
+		this.idNumber = idNumber;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.gpa = gpa;
+	}
 	
 	public String toString()
 	{
-		String str = "";
+		String str = "This person is not a current student.";
+		if(isCurrentStudent)
+		{
+			str = "Name: " + name + ", Sex: " + gender + ", Age: " + age + ", ID: " + idNumber + ", Grade Level: " + gradeLevel + ", GPA: " + gpa;
+		}
 		return str;
+		//this is an override
+		
 	}
 	
 }
--- BetterJPAMs/src/studentManagement/controller/StudentRunner.java
+++ BetterJPAMs/src/studentManagement/controller/StudentRunner.java
@@ -6,7 +6,7 @@ public class StudentRunner
 {
 	public static void main(String[] args)
 	{
-		Student myStudent = new Student();
+		Student myStudent = new Student(true, 12, 645677, "Zach Carter", "Undefined", 17, 4.0);
 		System.out.println(myStudent);
 	}
 }
