6239018f76ab2b26032c112663573d78f0c01361
ceb-student <CEB-Student@caddoschools.org>
2017-10-06 09:19:03 -0500

    added new classes
    added new classes

--- Craps/src/Die.java
+++ Craps/src/Die.java
@@ -1,14 +1,15 @@
 
 public class Die
 {
+	private int numDots;
+	
 	public void roll()
 	{
-		private int numDots =
+		numDots = (int)(Math.random()*6) + 1;
 	}
 	
-	public int getNumDots(int numDots)
+	public int getNumDots()
 	{
-	
 		return numDots;
 	}
 }
